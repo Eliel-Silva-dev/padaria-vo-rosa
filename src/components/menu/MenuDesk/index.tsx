39f0b32f7b4a31881f530d4ddd44c96ba74c3290
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-07-18 11:58:45 -0300

    feat: react padaria vo rosa componens menu menuDesk

---
+++ src/components/menu/MenuDesk/index.tsx
@@ -0,0 +1,24 @@
+import style from './style.min.module.css';
+
+import Link from 'next/link';
+
+const MenuDesk = () => {
+  return (
+    <ul className={style.menu_desk}>
+      <li>
+        <Link href={'/'}>Inicio</Link>
+      </li>
+      <li>
+        <Link href={'/'}>menu 01</Link>
+      </li>
+      <li>
+        <Link href={'/'}>menu 02</Link>
+      </li>
+      <li>
+        <Link href={'/'}>menu 03</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default MenuDesk;
