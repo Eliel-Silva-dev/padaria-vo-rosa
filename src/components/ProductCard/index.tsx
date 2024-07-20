5e814ebadf0ece0a2d5ad3dbde3aaf086236cc24
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-07-20 13:19:20 -0300

    feat: react padaria vo rosa component button details

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,3 +1,4 @@
+import ButtonDetails from '../buttons/ButtonDetails';
 import style from './style.min.module.css';
 
 type TProductCard = {
@@ -20,7 +21,7 @@ const ProductCard = ({
       <div className={style.info}>
         <h3>{nameProduct}</h3>
         <p>{price}</p>
-        <ButtonDetails id={id_prod} />
+        <ButtonDetails id_details={id_prod} />
       </div>
       <div className={style.img_prod_card}>
         <img src={srcImage} alt={altImage} />
---
+++ src/components/buttons/ButtonDetails/index.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import style from './style.min.module.css';
+
+type TButtonDetails = {
+  id_details: number;
+};
+const ButtonDetails = ({id_details}: TButtonDetails) => {
+  return (
+    <button className={style.button} type="button">
+      <Link
+        href={{
+          pathname: '/product',
+          query: { id: id_details },
+        }}
+      >
+        Detalhes
+      </Link>
+    </button>
+  );
+};
+
+export default ButtonDetails;
