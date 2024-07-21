daae999c8e593da703f213d270a9ba6abc729310
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-07-20 21:36:26 -0300

    feat: react padaria vo rosa page product v4

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,9 +1,49 @@
+'use client';
+
 import style from './style.min.module.css';
+import { useSearchParams } from 'next/navigation';
+import { TProduct, products } from '@/database';
+import { useEffect, useState } from 'react';
+import { formatCurrency } from '@/scripts';
 
 const Product = () => {
+  const [product, setProduct] = useState({} as TProduct);
+
+  const search = useSearchParams();
+
+  const id = search.get('id');
+
+  useEffect(() => {
+    products.forEach((prod) => {
+      if (prod.id.toString() == id) {
+        setProduct(prod);
+      }
+    });
+
+    console.log(product.Ingredients);
+  }, [id]);
   return (
     <main className={style.product}>
-      <h2>pagina de produto</h2>
+      {product && (
+        <>
+          <h2>Detalhes do produto: {product.title}</h2>
+          <section>
+            <div className={style.container_img}>
+              <img src={product.imgAbout} alt={product.title} />
+            </div>
+            <div className={style.container_info}>
+              <div>
+                <h3>Ingredientes: </h3>
+                {product.Ingredients &&
+                  product.Ingredients.map((item, idx) => (
+                    <p key={idx}>* {item}</p>
+                  ))}
+              </div>
+              <h3>Valor: {product.price && formatCurrency(product.price)}</h3>
+            </div>
+          </section>
+        </>
+      )}
     </main>
   );
 };
